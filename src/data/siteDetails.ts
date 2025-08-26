50b0f75923fd0fe6011ce8cfeb7604650ec4b247
ricardosantoss <87617282+ricardosantoss@users.noreply.github.com>
2025-08-25 22:23:02 -0300

    Create siteDetails.ts

---
+++ src/data/siteDetails.ts
@@ -0,0 +1,5 @@
+export const siteDetails = {
+  siteName: "Codex",
+  description: "Predição automática de CID-10 com explicabilidade e validação médica assistida.",
+  whatsapp: "https://wa.me/5562985499620?text=Ol%C3%A1,%20quero%20saber%20mais%20sobre%20a%20Codex"
+};
